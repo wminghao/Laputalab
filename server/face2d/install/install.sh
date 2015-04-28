8c7d1c2453e6bd698fa45ae647aafebe0e051c37
VisparCorp <ubuntu@vispar.com>
2015-04-28 00:06:43 +0000

    added a script to start and stop face2d server.

--- server/face2d/install/install.sh
+++ server/face2d/install/install.sh
@@ -1,8 +1,13 @@
 cd ../..
 scons
 cd face2d/install
-sudo cp ~/Laputalab/server/build/Linux-x86_64/face2d/prog/LandMarkMain /usr/bin/
+sudo cp face2d.sh /etc/init.d/
+sudo /etc/init.d/face2d.sh stop
+sudo cp /home/ubuntu/Laputalab/server/build/Linux-x86_64/face2d/prog/LandMarkMain /usr/bin/
 sudo chown root:root /usr/bin/LandMarkMain
 sudo chown -R root:root /usr/bin/Model
-sudo nohup ~/Laputalab/server/build/Linux-x86_64/face2d/prog/face2d_server &
-
+if [ ! -d "/usr/share/face2d/" ]; then
+    sudo mkdir /usr/share/face2d
+fi
+sudo cp /home/ubuntu/Laputalab/server/build/Linux-x86_64/face2d/prog/face2d_server /usr/share/face2d/
+sudo /etc/init.d/face2d.sh start
