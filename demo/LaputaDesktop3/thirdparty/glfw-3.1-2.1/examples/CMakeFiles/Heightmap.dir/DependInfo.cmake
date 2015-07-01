06f1aacd4037d5abd69e286772bf0c572e113b59
Howard <wminghao@yahoo.com>
2015-07-01 12:02:54 -0700

    added glfw source code for demo.

---
+++ demo/LaputaDesktop3/thirdparty/glfw-3.1-2.1/examples/CMakeFiles/Heightmap.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/howard/AR/demo/LaputaDesktop3/thirdparty/glfw-3.1-2.1/deps/glad.c" "/Users/howard/AR/demo/LaputaDesktop3/thirdparty/glfw-3.1-2.1/examples/CMakeFiles/Heightmap.dir/__/deps/glad.c.o"
+  "/Users/howard/AR/demo/LaputaDesktop3/thirdparty/glfw-3.1-2.1/examples/heightmap.c" "/Users/howard/AR/demo/LaputaDesktop3/thirdparty/glfw-3.1-2.1/examples/CMakeFiles/Heightmap.dir/heightmap.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "Clang")
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/howard/AR/demo/LaputaDesktop3/thirdparty/glfw-3.1-2.1/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "include"
+  "deps"
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
