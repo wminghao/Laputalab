b121118eb309516b725f702d6c8bc0dd5fa88993
wminghao <wminghao@yahoo.com>
2015-07-11 07:54:29 +0000

    adjusted the test case.

---
+++ demo/LinuxGL/test/runtest.sh
@@ -0,0 +1,4 @@
+time /Laputalab/demo/LinuxGL/build/Linux-x86_64/VET/prog/VETHeadless Tom.jpg Tomo.jpg purpleglasses2.obj
+time /Laputalab/demo/LinuxGL/build/Linux-x86_64/VET/prog/VETHeadless girl.jpg girlo.jpg blackglasses2.obj
+time /Laputalab/demo/LinuxGL/build/Linux-x86_64/VET/prog/VETHeadless girl2.jpg girl2o.jpg RanGlasses2.obj
+
--- demo/LinuxGL/test/runtwo.sh
+++
@@ -1,4 +0,0 @@
-time /Laputalab/demo/LinuxGL/build/Linux-x86_64/VET/prog/VETHeadless Tom.jpg Tomo.jpg purpleglasses.obj
-time /Laputalab/demo/LinuxGL/build/Linux-x86_64/VET/prog/VETHeadless girl.jpg girlo.jpg purpleglasses.obj
-time /Laputalab/demo/LinuxGL/build/Linux-x86_64/VET/prog/VETHeadless girl2.jpg girl2o.jpg RanGlasses.obj
-
