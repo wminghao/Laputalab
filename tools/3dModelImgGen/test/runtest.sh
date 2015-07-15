8d28b23ce3ef49a5294ca87004172bc16d96c5a7
wminghao <wminghao@yahoo.com>
2015-07-15 01:44:31 +0000

    rename file to small letter for linux.

--- tools/3dModelImgGen/test/runtest.sh
+++ tools/3dModelImgGen/test/runtest.sh
@@ -1,20 +1,4 @@
-cd ../result
-rm RanGlasses.tar.gz 
-rm -rf RanGlasses
-cd ../test
-./runsingle.sh RanGlasses +0
-./runsingle.sh RanGlasses +10
-./runsingle.sh RanGlasses +20
-./runsingle.sh RanGlasses +30
-./runsingle.sh RanGlasses +40
-./runsingle.sh RanGlasses +50
-./runsingle.sh RanGlasses +60
-./runsingle.sh RanGlasses -10
-./runsingle.sh RanGlasses -20
-./runsingle.sh RanGlasses -30
-./runsingle.sh RanGlasses -40
-./runsingle.sh RanGlasses -50
-./runsingle.sh RanGlasses -60
-cd ../result
-tar -zcvf RanGlasses.tar.gz RanGlasses
-cd ../test
+#!/bin/bash
+#./runpanaroma.sh purpleglasses
+#./runpanaroma.sh RanGlasses
+./runpanaroma.sh blackGlasses
