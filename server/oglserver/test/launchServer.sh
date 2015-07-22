dbed6fb201cbbabe1e9ab786c9b474f08c1262c6
wminghao <wminghao@yahoo.com>
2015-07-22 23:44:58 +0000

    added test 2.

--- server/oglserver/test/launchServer.sh
+++ server/oglserver/test/launchServer.sh
@@ -1,8 +1,5 @@
 cd ../..
 scons type=debug
 cd oglserver/test
-#sudo cp /Laputalab/server/build/Linux-x86_64/face2d/prog/LandMarkMain /usr/bin/
-#sudo chown root:root /usr/bin/LandMarkMain
-#sudo chown -R root:root /usr/bin/Model
 sudo /Laputalab/server/build/Linux-x86_64/oglserver/prog/ogl_server
 
--- server/oglserver/test/launchValgrindServer.sh
+++ server/oglserver/test/launchValgrindServer.sh
@@ -1,7 +1,4 @@
 cd ../..
 scons type=debug
 cd oglserver/test
-#sudo cp ~/Laputalab/server/build/Linux-x86_64/face2d/prog/LandMarkMain /usr/bin/
-#sudo chown root:root /usr/bin/LandMarkMain
-#sudo chown -R root:root /usr/bin/Model
 sudo valgrind --leak-check=yes --tool=memcheck ~/Laputalab/server/build/Linux-x86_64/oglserver/prog/ogl_server
